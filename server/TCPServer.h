252060c140e6c9558dbd384475b5f37769f5e9ac
Wshido <rfox2410@gmail.com>
2026-05-02 23:32:48 +0300

    server

---
+++ server/TCPServer.h
@@ -0,0 +1,19 @@
+﻿#ifndef TCP_SERVER_H
+#define TCP_SERVER_H
+#include "pch.h"
+
+class TCPServer
+{
+public:
+	int sockfd;
+	struct sockaddr_in serverAddress;
+	struct sockaddr_in clientAddress;
+	void setup(int port);
+	string receive();
+	void Send(int clientSock, string msg);
+	void detach();
+
+private:
+	static void Task(int clientSock);
+};
+#endif
